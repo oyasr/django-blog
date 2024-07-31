328401de797e12038448c000f8aaa418b21a14f3
Osama Yasser <osamayasserr@gmail.com>
2024-07-31 21:55:40 +0300

    feat: Add author field to the Post model and make migration
    This commit adds an `author` field to the `Post` model in the 'blog' app. The `author` field is a foreign key to the `AUTH_USER_MODEL` and is used to associate each blog post with its author.

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -14,6 +15,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
+    )
 
     class Meta:
         ordering = ["-publish"]
