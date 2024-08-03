5fb6f1417b5af1f5f96244a95cca5ed3e593a1d7
Osama Yasser <osamayasserr@gmail.com>
2024-08-03 05:29:36 +0300

    feat: Add blog app URLs and views for post list and detail views

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
+from django.http import HttpRequest
 
-# Create your views here.
+
+def post_list(request: HttpRequest):
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request: HttpRequest, id: int):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, "blog/post/detail.html", {"post": post})
