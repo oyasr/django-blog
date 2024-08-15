614285cad157f6329a832ff4801495c079e9e9e7
Osama Yasser <osamayasserr@gmail.com>
2024-08-15 23:27:23 +0300

    feat: Add pagination to blog post list view
    The code changes introduce pagination functionality to the blog post list view. This allows users to navigate through multiple pages of blog posts. The pagination is implemented using a new template `pagination.html` and is included in the `list.html` template. The `PostListView` class-based view is updated to use the `Paginator` class from Django's `core.paginator` module. The number of posts per page is set to 1. This enhancement improves the user experience by breaking up long lists of blog posts into manageable pages.

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, PostListView
 
 
 app_name = "blog"
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
+    path("", PostListView.as_view(), name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:slug>/", post_detail, name="post_detail"
     ),
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,10 +1,22 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.http import HttpRequest
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 1
+    template_name = "blog/post/list.html"
 
 
 def post_list(request: HttpRequest):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 1)
+    page_number = request.GET.get("page", 1)
+    posts = paginator.get_page(page_number)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
